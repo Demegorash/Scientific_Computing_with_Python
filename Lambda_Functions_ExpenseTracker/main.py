fe1c7840df3cb363bafd5a94f5d5505a1e2ef002
Demegorash <felipe.solisgranados@gmail.com>
2025-05-31 14:38:39 -0600

    Learning Python Lambda Functions with a Expense Tracker.  Steps 01-15

--- Lambda_Functions_ExpenseTracker/main.py
+++ Lambda_Functions_ExpenseTracker/main.py
@@ -1,3 +1,7 @@
+''' 
+
+This will be theory.  After step 8 we need to remove it, however we are going to comment, but from 7 to begginging this was not commented.
+
 my_list = [1,2]
 
 my_list.append(3)
@@ -7,3 +11,20 @@ print(my_list[0])
 
 my_list[0] = 0
 print(my_list)
+
+my_list.insert(1, 1)
+print(my_list)
+
+my_list.pop()
+print(my_list)
+
+'''
+
+def add_expense(expenses, amount, category):
+    expenses.append({"amount" : amount, "category" : category})
+
+def print_expenses(expenses):
+    for expense in expenses:
+        pass
+
+expenses = []
